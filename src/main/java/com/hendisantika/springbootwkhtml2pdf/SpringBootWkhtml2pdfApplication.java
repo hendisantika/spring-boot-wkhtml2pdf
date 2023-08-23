edcbcb1ba707140e7bf62f291cca0ac47d8c777a
hendisantika <hendisantika@yahoo.co.id>
2023-08-23 12:12:56 +0700

    #1 - Initial Commit ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootwkhtml2pdf/SpringBootWkhtml2pdfApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwkhtml2pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWkhtml2pdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWkhtml2pdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootwkhtml2pdf/SpringBootWkhtml2pdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwkhtml2pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWkhtml2pdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
