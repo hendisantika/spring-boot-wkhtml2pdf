31889d0eca4d3d524250516ede8b24f4b1f47ac3
hendisantika <hendisantika@yahoo.co.id>
2023-08-23 12:36:48 +0700

    #6 - Add StudentService ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootwkhtml2pdf/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springbootwkhtml2pdf.service;
+
+import com.hendisantika.springbootwkhtml2pdf.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-wkhtml2pdf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/23/23
+ * Time: 12:36
+ * To change this template use File | Settings | File Templates.
+ */
+@Service("studentService")
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+}
